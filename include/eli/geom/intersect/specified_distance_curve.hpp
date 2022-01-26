c10659e84683f36d121b49a94c23566b0fa1f691
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-01-26 15:58:05 -0800

    Make specified_distance_curve take bounds for NRM
    The user can now specify a region of a curve to search for a specified
    distance.
    
    This removes special behavior for periodic curves.

--- include/eli/geom/intersect/specified_distance_curve.hpp
+++ include/eli/geom/intersect/specified_distance_curve.hpp
@@ -22,6 +22,7 @@
 
 #include "eli/mutil/nls/iterative_root_base_constrained.hpp"
 #include "eli/mutil/nls/bisection_method.hpp"
+#include "eli/mutil/nls/newton_raphson_method.hpp"
 
 #include "eli/geom/point/distance.hpp"
 #include "eli/geom/curve/piecewise.hpp"
@@ -104,7 +105,7 @@ namespace eli
       }
 
       template<typename curve__>
-      typename curve__::data_type specified_distance(typename curve__::data_type &t, const curve__ &c, const typename curve__::point_type &pt, const typename curve__::data_type &r0, const typename curve__::data_type &t0)
+      typename curve__::data_type specified_distance(typename curve__::data_type &t, const curve__ &c, const typename curve__::point_type &pt, const typename curve__::data_type &r0, const typename curve__::data_type &t0, const typename curve__::data_type &tmin, const typename curve__::data_type &tmax )
       {
         eli::mutil::nls::newton_raphson_method<typename curve__::data_type> nrm;
         internal::curve_spec_g_functor<curve__> g;
@@ -123,15 +124,9 @@ namespace eli
         // setup the solver
         nrm.set_absolute_f_tolerance(tol.get_absolute_tolerance());
         nrm.set_max_iteration(10);
-        if (c.open())
-        {
-          nrm.set_lower_condition(c.get_t0(), eli::mutil::nls::newton_raphson_method<typename curve__::data_type>::IRC_EXCLUSIVE);
-          nrm.set_upper_condition(c.get_tmax(), eli::mutil::nls::newton_raphson_method<typename curve__::data_type>::IRC_EXCLUSIVE);
-        }
-        else
-        {
-          nrm.set_periodic_condition(c.get_t0(), c.get_tmax());
-        }
+
+        nrm.set_lower_condition(tmin, eli::mutil::nls::newton_raphson_method<typename curve__::data_type>::IRC_EXCLUSIVE);
+        nrm.set_upper_condition(tmax, eli::mutil::nls::newton_raphson_method<typename curve__::data_type>::IRC_EXCLUSIVE);
 
         // set the initial guess
         nrm.set_initial_guess(t0);
@@ -154,6 +149,12 @@ namespace eli
         // couldn't find better answer so return initial guess
         t=t0;
         return dist0;
+      }
+
+      template<typename curve__>
+      typename curve__::data_type specified_distance(typename curve__::data_type &t, const curve__ &c, const typename curve__::point_type &pt, const typename curve__::data_type &r0, const typename curve__::data_type &t0 )
+      {
+        return specified_distance(t, c, pt, r0, t0, c.get_t0(), c.get_tmax() );
       }
 
       template<typename curve__>
