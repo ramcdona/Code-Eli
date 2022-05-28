356a962768f883a5a0e460f53860632823010b42
Rob McDonald <rob.a.mcdonald@gmail.com>
2022-05-27 23:22:11 -0700

    Implement other approaches to Find RST

--- include/eli/geom/intersect/find_rst_surface.hpp
+++ include/eli/geom/intersect/find_rst_surface.hpp
@@ -288,6 +288,90 @@ namespace eli
       }
 
       template<template<typename, unsigned short, typename> class surface__, typename data__, unsigned short dim__, typename tol__ >
+      typename surface::piecewise<surface__, data__, dim__, tol__>::data_type find_rst_no_bbox(
+                typename surface::piecewise<surface__, data__, dim__, tol__>::data_type &r,
+                typename surface::piecewise<surface__, data__, dim__, tol__>::data_type &s,
+                typename surface::piecewise<surface__, data__, dim__, tol__>::data_type &t,
+                const surface::piecewise<surface__, data__, dim__, tol__> &ps,
+                const typename surface::piecewise<surface__, data__, dim__, tol__>::point_type &pt,
+                typename surface::piecewise<surface__, data__, dim__, tol__>::index_type &ret )
+      {
+        typedef surface::piecewise<surface__, data__, dim__, tol__> piecewise_type;
+        typedef typename piecewise_type::index_type index_type;
+        typedef typename piecewise_type::data_type data_type;
+        typedef typename piecewise_type::bounding_box_type bounding_box_type;
+        typedef typename piecewise_type::surface_type patch_type;
+        typedef typename surface::piecewise<surface__, data__, dim__, tol__>::point_type point_type;
+
+
+        bool dbg = false;
+
+
+        data_type r0 = 0.5;
+        data_type s0 = 0.25;
+        data_type t0 = 0.5;
+        data_type dist;
+
+        dist = find_rst( r, s, t, ps, pt, r0, s0, t0, ret );
+
+
+        if ( ret != 0 ) // Not converged.  Try surface projection.
+        {
+          data_type u, v, d;
+          d = minimum_distance( u, v, ps, pt );
+
+          if ( dbg )
+          {
+            std::cout << "Resorted to surface projection." << std::endl;
+          }
+
+          if( d < dist ) // Check for improvement.
+          {
+            data_type vmin, vmax;
+            data_type umin, umax;
+            data_type urng, vrng;
+
+            ps.get_parameter_min( umin, vmin );
+            ps.get_parameter_max( umax, vmax );
+            urng = umax - umin;
+            vrng = vmax - vmin;
+
+            data_type rr = ( u - umin ) / urng;
+            data_type ss = ( v - vmin ) / vrng;
+            data_type tt = 0.0; // Assume on bottom surface.
+
+            if ( ss > 0.5 ) // Actually on top surface.
+            {
+              ss = 1.0 - ss;
+              tt = 1.0;
+            }
+            index_type rret = 0;
+
+            if ( dbg )
+            {
+              std::cout << "Surface projection success." << std::endl;
+              std::cout << "d " << d << std::endl << std::endl << std::endl;
+            }
+
+            dist = d;
+            r = rr;
+            s = ss;
+            t = tt;
+            ret = rret;
+          }
+          else
+          {
+            if ( dbg )
+            {
+              std::cout << "Surface projection failure." << std::endl;
+            }
+          }
+        }
+
+        return dist;
+      }
+
+      template<template<typename, unsigned short, typename> class surface__, typename data__, unsigned short dim__, typename tol__ >
       typename surface::piecewise<surface__, data__, dim__, tol__>::data_type find_rst(
           typename surface::piecewise<surface__, data__, dim__, tol__>::data_type &r,
           typename surface::piecewise<surface__, data__, dim__, tol__>::data_type &s,
@@ -493,6 +577,265 @@ namespace eli
 
         return dist;
       }
+
+      template<template<typename, unsigned short, typename> class surface__, typename data__, unsigned short dim__, typename tol__ >
+      void find_rst_old( std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &r,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &s,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &t,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &d,
+                     const surface::piecewise<surface__, data__, dim__, tol__> &ps,
+                     const std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::point_type > &pt,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::index_type > & ret )
+      {
+        typedef surface::piecewise<surface__, data__, dim__, tol__> piecewise_type;
+        typedef typename piecewise_type::index_type index_type;
+        index_type i, n( pt.size() );
+        r.resize( n );
+        s.resize( n );
+        t.resize( n );
+        d.resize( n );
+        ret.resize( n );
+
+        for ( i = 0; i < n; i++ )
+        {
+          d[i] = find_rst( r[i], s[i], t[i], ps, pt[i], ret[i] );
+        }
+      }
+
+      template<template<typename, unsigned short, typename> class surface__, typename data__, unsigned short dim__, typename tol__ >
+      void find_rst( std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &r,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &s,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &t,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::data_type > &d,
+                     const surface::piecewise<surface__, data__, dim__, tol__> &ps,
+                     const std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::point_type > &pt,
+                     std::vector < typename surface::piecewise<surface__, data__, dim__, tol__>::index_type > & ret )
+      {
+        typedef surface::piecewise<surface__, data__, dim__, tol__> piecewise_type;
+        typedef typename piecewise_type::index_type index_type;
+        typedef typename piecewise_type::data_type data_type;
+        typedef typename piecewise_type::bounding_box_type bounding_box_type;
+        typedef typename piecewise_type::surface_type patch_type;
+        typedef typename surface::piecewise<surface__, data__, dim__, tol__>::point_type point_type;
+
+        typedef std::pair<data_type, data_type> pdata;
+        typedef std::pair<index_type, index_type> iuivdata;
+        typedef std::vector< std::pair<data_type, iuivdata > > dvec;
+
+        bool dbg = false;
+
+        data_type vmin, vmid, vmax;
+        data_type umin, umax;
+        data_type urng, vrng;
+        piecewise_type lower, upper;
+
+        ps.get_parameter_min( umin, vmin );
+        ps.get_parameter_max( umax, vmax );
+        urng = umax - umin;
+        vrng = vmax - vmin;
+
+        vmid = 0.5 * ( vmin + vmax );
+
+        ps.split_v( lower, upper, vmid );
+        upper.reverse_v();
+        upper.set_v0( vmin );
+
+        piecewise_type::parm_match_u( lower, upper );
+        piecewise_type::parm_match_v( lower, upper );
+
+        // Not needed as no in-depth control point manipulations happen.
+        // piecewise_type::order_match_u( lower, upper );
+        // piecewise_type::order_match_v( lower, upper );
+
+        index_type nu = lower.number_u_patches();
+        index_type nv = lower.number_v_patches();
+
+        std::vector < std::vector < bounding_box_type > > bbmat( nu );
+
+        for( index_type i = 0; i < nu; i++ )
+        {
+          bbmat[i].resize( nv );
+          for( index_type j = 0; j < nv; j++ )
+          {
+            patch_type *plow = lower.get_patch( i, j );
+            patch_type *pup = upper.get_patch( i, j );
+
+            bounding_box_type bb_up;
+            plow->get_bounding_box( bbmat[i][j] );
+            pup->get_bounding_box( bb_up );
+            bbmat[i][j].add( bb_up );
+          }
+        }
+
+        index_type npt( pt.size() );
+
+        r.resize( npt );
+        s.resize( npt );
+        t.resize( npt );
+        d.resize( npt );
+        ret.resize( npt );
+
+        for ( index_type ipt = 0; ipt < npt; ipt++ )
+        {
+          dvec minbbdist;
+
+        // std::cout << "Number of patches " << nu * nv << std::endl;
+
+        // Find closest corner of bounding boxes, add them to vector
+        // Simple linear search, would be more efficient with some sort of tree.
+        for( index_type i = 0; i < nu; i++ )
+        {
+          for( index_type j = 0; j < nv; j++ )
+          {
+            data_type dbbmin;
+            dbbmin = minimum_distance( bbmat[i][j], pt[ipt] );
+
+            if ( dbg )
+            {
+              std::cout << "i " << i << " j " << j << " d " << dbbmin << std::endl;
+            }
+
+            minbbdist.push_back( std::make_pair( dbbmin, std::make_pair( i, j ) ) );
+          }
+        }
+
+        // std::cout << std::endl << std::endl;
+
+        // Sort by nearest distance.
+        std::sort( minbbdist.begin(), minbbdist.end(), pairfirstcompare< data_type, iuivdata > );
+
+        if ( dbg )
+        {
+            std::cout << "Number of trial points " << minbbdist.size() << std::endl;
+            std::cout << "Largest d0 " << (minbbdist.back()).first << std::endl;
+        }
+
+        data_type dist( std::numeric_limits< data_type >::max() );
+
+        index_type count = 0;
+        index_type improve = 0;
+        typename dvec::const_iterator it;
+        for ( it = minbbdist.begin(); it != minbbdist.end(); ++it )
+        {
+          // std::cout << "count " << count << " d0 " << it->first << " dist " << dist << std::endl;
+
+          // If current best is farther than the next nearest bb distance, try again
+          // Place sqrt(eps) tolerance on bb distance so we don't keep trying when we've achieved
+          // 1e-16, but the bb distance is 0.0.
+          // It is likely that the first few bb distances will be 0.0 as multiple bounding boxes
+          // may contain our point of interest.
+          if( dist > ( it->first + std::sqrt( std::numeric_limits< data_type >::epsilon() ) ) )
+          {
+            data_type dbb = it->first;
+            iuivdata iuiv = it->second;
+
+            index_type iu = iuiv.first;
+            index_type iv = iuiv.second;
+
+            data_type ustart, du, vstart, dv;
+
+            patch_type *plow = lower.get_patch( iu, iv, ustart, du, vstart, dv );
+            // patch_type *pup = upper.get_patch( iu, iv );
+
+            data_type u = ustart + 0.5 * du;
+            data_type v = vstart + 0.5 * dv;
+
+            data_type r0 = ( u - umin ) / urng;
+            data_type s0 = ( v - vmin ) / vrng;
+            data_type t0 = 0.5;
+
+            data_type rmin = ( ustart - umin ) / urng;
+            data_type rmax = ( ustart + du - umin ) / urng;
+
+            data_type smin = ( vstart - vmin ) / vrng;
+            data_type smax = ( vstart + dv - vmin ) / vrng;
+
+            data_type rr, ss, tt;
+            data_type dout;
+            index_type rret;
+
+//            dout = find_rst( rr, ss, tt, ps, pt[ipt], r0, s0, t0, rmin, rmax, smin, smax, rret );
+            dout = find_rst( rr, ss, tt, ps, pt[ipt], r0, s0, t0, rret );
+
+            if ( dbg )
+            {
+                std::cout << "iu " << iu << " iv " << iv << " dbb " << dbb << std::endl;
+                std::cout << "Target x " << pt[ipt].x() << " y " << pt[ipt].y() << " z " << pt[ipt].z() << std::endl;
+                std::cout << "rmin: " << rmin << " r0: " << r0 << " rmax: " << rmax << std::endl;
+                std::cout << "smin: " << smin << " s0: " << s0 << " smax: " << smax << std::endl;
+                std::cout << "t0: " << t0 << std::endl;
+                std::cout << "d " << dout << " rret " << rret << std::endl << std::endl << std::endl;
+            }
+
+            if( dout < dist ) // 0 means converged.
+            {
+              dist = dout;
+              d[ipt] = dist;
+              r[ipt] = rr;
+              s[ipt] = ss;
+              t[ipt] = tt;
+              ret[ipt] = rret;
+              improve++;
+            }
+          }
+          else
+          {
+            break;
+          }
+          count++;
+        }
+
+        //std::cout << "count: " << count << " improve: " << improve << " of: " << nu*nv << std::endl;
+
+        if ( ret[ipt] != 0 ) // Not converged.  Try surface projection.
+        {
+          data_type u, v, dout;
+          dout = minimum_distance( u, v, ps, pt[ipt] );
+
+          if ( dbg )
+          {
+            std::cout << "Resorted to surface projection." << std::endl;
+          }
+
+          if( dout < dist ) // Check for improvement.
+          {
+            data_type rr = ( u - umin ) / urng;
+            data_type ss = ( v - vmin ) / vrng;
+            data_type tt = 0.0; // Assume on bottom surface.
+
+            if ( ss > 0.5 ) // Actually on top surface.
+            {
+              ss = 1.0 - ss;
+              tt = 1.0;
+            }
+            index_type rret = 0;
+
+            if ( dbg )
+            {
+              std::cout << "Surface projection success." << std::endl;
+              std::cout << "d " << dout << std::endl << std::endl << std::endl;
+            }
+
+            dist = dout;
+            d[ipt] = dist;
+            r[ipt] = rr;
+            s[ipt] = ss;
+            t[ipt] = tt;
+            ret[ipt] = rret;
+          }
+          else
+          {
+            if ( dbg )
+            {
+              std::cout << "Surface projection failure." << std::endl;
+            }
+          }
+        }
+
+        }
+      }
+
+
 
     }
   }
